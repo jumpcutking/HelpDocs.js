ca7d0de23bf4e33894a679bd7c089991a79dad0d
thanos <thanos@thanos.local>
2020-08-14 20:39:04 -0400

    Upgrade to Latest build

---
+++ app.js
@@ -0,0 +1,23 @@
+// staging.js
+var deployd = require('deployd');
+
+var server = deployd({
+  port: 8083,
+  env: 'staging',
+  db: {
+    connectionString: "mongodb://usr:pass@127.0.0.1:27017/udocs"
+  }
+});
+
+
+server.listen();
+
+server.on('error', function(err) {
+  console.error(err);
+  process.nextTick(function() { // Give the server a chance to return an error
+    process.exit();
+  });
+});
+
+//
+//
